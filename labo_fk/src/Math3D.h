cac6e534b9ff383f9a0a5bafc344191a3ffced8a
Phan Tung Bui <phan-tung.bui.1@ens.etsmtl.ca>
2026-02-12 21:45:46 -0500

    feat : complete Math3D

--- labo_fk/src/Math3D.h
+++ labo_fk/src/Math3D.h
@@ -4,12 +4,12 @@
  * @file Math3D.h
  *
  * @brief Fonctions pour l'intinialisation et la manipulation de matrices de
- * rotation, des matrices de transformations en coordonnées homogènes et les
+ * rotation, des matrices de transformations en coordonnï¿½es homogï¿½nes et les
  * vecteurs 3D.
  *
- * Nom: 
- * Code permanent : 
- * Email : 
+ * Nom: Phan Tung Bui
+ * Code permanent : BUIP26109708
+ * Email : phan-tung.bui.1@ens.etsmtl.ca
  *
  */
 
@@ -29,79 +29,172 @@ namespace gti320 {
     // Type de vecteur 3D.
     typedef Vector<float, 3> Vector3f;
 
-    // Dans le cadre de ce projet, nous considérons seulement deux
+    // Dans le cadre de ce projet, nous considï¿½rons seulement deux
     // cas :
     //
     //  - les rotations
     //  - les translations
     //
-    // Matrice en coordonnées homogèes :
+    // Matrice en coordonnï¿½es homogï¿½es :
     typedef Matrix<float, 4, 4, ColumnStorage> Matrix4f;
     // 
     // Matrice pour les rotations
     typedef Matrix<float, 3, 3, ColumnStorage> Matrix3f;
 
     /**
-     * Initialise et retourne la matrice identité
+     * Initialise et retourne la matrice identitï¿½
      */
     template<>
     inline void Matrix4f::setIdentity()
     {
-        // TODO affecter la valeur 0.0 partout, sauf sur la diagonale principale où c'est 1.0.
-        //      Note: ceci est une redéfinition d'une fonction membre!
+        // TODO affecter la valeur 0.0 partout, sauf sur la diagonale principale oï¿½ c'est 1.0.
+        //      Note: ceci est une redï¿½finition d'une fonction membre!
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                (*this)(i, j) = (i == j) ? 1: 0;
+            }
+        }
     }
 
     /**
-     * Calcul de la matrice inverse, SPÉCIALISÉ pour le cas d'une matrice de
-     * transformation rigide en coordonnées homogènes.
+     * Calcul de la matrice inverse, SPï¿½CIALISï¿½ pour le cas d'une matrice de
+     * transformation rigide en coordonnï¿½es homogï¿½nes.
      *
      * TODO (vous pouvez supposer qu'il s'agit d'une matrice de transformation
-     * en coordonnées homogènes)
+     * en coordonnï¿½es homogï¿½nes)
      */
     template<>
     inline Matrix4f Matrix4f::inverse() const
     {
-        // TODO : implémenter
-        return Matrix4f(); // Pas bon, à changer
+        // TODO : implï¿½menter
+        // A reviser
+        Matrix4f inv;
+        inv.setIdentity();
+
+        // Extraire R (3x3) et t (3x1)
+        Matrix3f R;
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                R(i, j) = (*this)(i, j);
+            }
+        }
+
+        Vector3f t;
+        t(0) = (*this)(0, 3);
+        t(1) = (*this)(1, 3);
+        t(2) = (*this)(2, 3);
+
+        // R^T
+        Matrix3f Rt;
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                Rt(i, j) = R(j, i);
+            }
+        }
+
+        // Placer Rt dans inv
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                inv(i, j) = Rt(i, j);
+            }
+        }
+
+        // Calculer -Rt * t
+        Vector3f negRtt;
+        for (int i = 0; i < 3; ++i) {
+            float s = 0.0f;
+            for (int k = 0; k < 3; ++k) {
+                s += Rt(i, k) * t(k);
+            }
+            negRtt(i) = -s;
+        }
+
+        inv(0, 3) = negRtt(0);
+        inv(1, 3) = negRtt(1);
+        inv(2, 3) = negRtt(2);
+
+        // DerniÃ¨re ligne (0 0 0 1)
+        inv(3, 0) = 0.0f; inv(3, 1) = 0.0f; inv(3, 2) = 0.0f; inv(3, 3) = 1.0f;
+
+        return inv;
     }
     
     /**
-     * Calcul de la matrice inverse, SPÉCIALISÉ pour le cas d'une matrice de rotation.
+     * Calcul de la matrice inverse, SPï¿½CIALISï¿½ pour le cas d'une matrice de rotation.
      *
      * (vous pouvez supposer qu'il s'agit d'une matrice de rotation)
      */
     template<>
     inline Matrix3f Matrix3f::inverse() const
     {
-        // TODO : implémenter
-        return Matrix3f();
+        // TODO : implï¿½menter
+        Matrix3f Rt;
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                Rt(i, j) = (*this)(j, i);
+            }
+        }
+        return Rt;
     }
 
 
     /**
-     * Multiplication d'une matrice 4x4 avec un vecteur 3D où la matrice
-     * représente une transformation rigide en coordonnées homogène.
+     * Multiplication d'une matrice 4x4 avec un vecteur 3D oï¿½ la matrice
+     * reprï¿½sente une transformation rigide en coordonnï¿½es homogï¿½ne.
      */
     template <typename _Scalar>
     Vector<_Scalar, 3> operator*(const Matrix<_Scalar, 4, 4, ColumnStorage>& A, const Vector<_Scalar, 3>& v)
     {
-        // TODO : implémenter
-        return Vector<_Scalar, 3>(); // pas bon, à changer
+        // TODO : implï¿½menter
+        Vector<_Scalar, 3> result;
+
+        for (int i = 0; i < 3; ++i) {
+            _Scalar s = 0;
+            for (int j = 0; j < 3; ++j) {
+                s += A(i, j) * v(j);
+            }
+            s += A(i, 3);
+            result(i) = s;
+        }
+
+        return result;
     }
 
 
     /**
-     * Initialise et retourne la matrice de rotation définie par les angles
-     * d'Euler XYZ exprimés en radians.
+     * Initialise et retourne la matrice de rotation dï¿½finie par les angles
+     * d'Euler XYZ exprimï¿½s en radians.
      *
      * La matrice doit correspondre au produit : Rz*Ry*Rx.
      */
     template<typename _Scalar>
     static Matrix<_Scalar, 3, 3> makeRotation(_Scalar x, _Scalar y, _Scalar z)
     {
-        // TODO : implémenter
+        // TODO : implï¿½menter
+        //Diapo 29
+        const _Scalar cos_x = cos(x), sin_x = sin(x);
+        const _Scalar cos_y = cos(y), sin_y = sin(y);
+        const _Scalar cos_z = cos(z), sin_z = sin(z);
 
-        return Matrix<_Scalar, 3, 3>(); //	 pas bon, à changer
+        Matrix<_Scalar, 3, 3> Rx, Ry, Rz;
+
+        // Rx
+        Rx(0,0)=1;  Rx(0,1)=0;      Rx(0,2)=0;
+        Rx(1,0)=0;  Rx(1,1)=cos_x;  Rx(1,2)=-sin_x;
+        Rx(2,0)=0;  Rx(2,1)=sin_x;  Rx(2,2)=cos_x;
+
+        // Ry
+        Ry(0,0)=cos_y;  Ry(0,1)=0;  Ry(0,2)=sin_y;
+        Ry(1,0)=0;      Ry(1,1)=1;  Ry(1,2)=0;
+        Ry(2,0)=-sin_y; Ry(2,1)=0;  Ry(2,2)=cos_y;
+
+        // Rz
+        Rz(0,0)=cos_z;  Rz(0,1)=-sin_z; Rz(0,2)=0;
+        Rz(1,0)=sin_z;  Rz(1,1)=cos_z;  Rz(1,2)=0;
+        Rz(2,0)=0;      Rz(2,1)=0;      Rz(2,2)=1;
+
+        // Produit: Rz * Ry * Rx
+        return Rz * (Ry * Rx);
     }
 
 }
